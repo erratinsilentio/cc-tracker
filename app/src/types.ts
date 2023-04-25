ceb465f3dc9c8b04b4ed95b91638566ffd27e2c8
erratinsilentio <kacper.kondracki@wp.pl>
2023-04-25 11:39:34 +0200

    started working on types

---
+++ app/src/types.ts
@@ -0,0 +1,32 @@
+export interface User {
+  id: string;
+  email: string;
+  workoutNumber: number;
+}
+
+export enum Exercise {
+  Pushup = "pushup",
+  Squat = "squat",
+  Pullup = "pullup",
+  LegRaise = "leg raise",
+  Bridge = "bridge",
+  Handstand = "handstand",
+}
+
+export interface Workout {
+  id: string;
+  dateCreated: string;
+  dateDone: string;
+  userId: string;
+  series: number;
+  exercises: { name: Exercise; level: number; repetitions: [number] };
+}
+
+export enum Paths {
+  Home = "/",
+  Exercises = "/exercises",
+  Workouts = "/workouts",
+  AddWorkout = "/workouts/add",
+  EditWorkout = "/workouts/edit",
+  Login = "/login",
+}
