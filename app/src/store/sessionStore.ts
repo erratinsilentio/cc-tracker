bf86bacf6a0653ba8feddd8add8d90847df2f777
erratinsilentio <kacper.kondracki@wp.pl>
2023-04-25 21:37:08 +0200

    added pinia store for storing sessions

---
+++ app/src/store/sessionStore.ts
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+import { Session } from "@supabase/gotrue-js/src/lib/types";
+
+export const useSessionStore = defineStore("session", {
+  state: () => ({ session: null as Session | null }),
+  actions: {
+    setSession(session: Session) {
+      this.session = session;
+    },
+    getSession() {
+      return this.session;
+    },
+  },
+});
